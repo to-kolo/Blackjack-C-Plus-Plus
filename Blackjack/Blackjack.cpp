39cffaf5b373a9bc901034af6d11ef2374e83a11
tkolo <tkolo@TOMMY-LAPTOP>
2022-05-24 18:06:17 -0400

    Add project files.

---
+++ Blackjack/Blackjack.cpp
@@ -0,0 +1,91 @@
+﻿#include "Blackjack.h"
+
+Card givePlayerCardFromDealer(Player p, Dealer d) {
+	Card c = d.removeCard();
+	p.addCard(c);
+	return c;
+}
+
+void dealCards(Dealer d, Player p) {
+
+}
+
+void displaySum(Player p) {
+	std::cout << "Sum: ";
+	if (p.getAceSum() > 0) {
+		std::cout << p.getAceSum() << "/" << p.getSum() << std::endl;
+	}
+	else {
+		std::cout << p.getSum() << std::endl;
+	}
+}
+
+int dealerTurn(Dealer d) {
+	Card firstDealerCard = d.giveCardToDealer();
+	Card secondDealerCard = d.giveCardToDealer();
+	std::cout << "The dealer's first card is the ";
+	firstDealerCard.readCard();
+	std::cout << "The dealer's second card is the ";
+	secondDealerCard.readCard();
+	return d.getSum();
+}
+
+bool determineGame(int dealerSum, int playerSum) {
+
+}
+
+void beginSinglePlayer() {
+	Dealer d(10);
+	float money = 50.00;
+	Player p(money);
+	while (p.getMoney() > 0) {
+		Card firstPlayerCard = givePlayerCardFromDealer
+		bool isIn = true;
+		while (isIn) {
+			std::cout << "Would you like to hit or stay?" << std::endl;
+			std::string hitOrStay;
+			std::cin >> hitOrStay;
+			if (hitOrStay == "Hit") {
+				Card nextCard = givePlayerCardFromDealer(p, d);
+				std::cout << "The next card is the ";
+				nextCard.readCard();
+				if (p.getSum() > 21) {
+					std::cout << "Busted!" << std::endl;
+					isIn = false;
+				}
+				else {
+					displaySum(p);
+				}
+			}
+			else if (hitOrStay == "Stay") {
+				int dealerSum = dealerTurn(d);
+				if (determineGame(dealerSum, p.getSum())) {
+
+				}
+			}
+		}
+	}
+}
+
+void beginMultiPlayer() {
+
+}
+
+int main() {
+	std::cout << "Welcome To Blackjack!" << std::endl;
+	std::cout << "Please select an option: " << std::endl;
+	std::cout << "1. Single Player" << std::endl;
+	std::cout << "2. Multiplayer" << std::endl;
+	std::string firstOption;
+	std::cin >> firstOption;
+	if (firstOption == "1") {
+		beginSinglePlayer();
+	}
+	else if (firstOption == "2") {
+		beginMultiPlayer();
+	}
+	else {
+
+	}
+	return 0; 
+}
---
+++ Blackjack/Blackjack.h
@@ -0,0 +1,156 @@
+﻿// Blackjack.h : Include file for standard system include files,
+// or project specific include files.
+
+#include <iostream>
+#include <queue>
+#include <array>
+/// <summary>
+///		Converts the card number into an int for scoring.
+/// </summary>
+/// <param name="num"></param>
+/// <returns>the resulting int value for scoring</returns>
+int cardToInt(std::string num) {
+	
+	if (num == "Two") return 2;
+	else if (num == "Three") return 3;
+	else if (num == "Four") return 4;
+	else if (num == "Five") return 5;
+	else if (num == "Six") return 6;
+	else if (num == "Seven") return 7;
+	else if (num == "Eight") return 8;
+	else if (num == "Nine") return 9;
+	else if (num == "Ten") return 10;
+	else if (num == "Jack") return 10;
+	else if (num == "Queen") return 10;
+	else if (num == "King") return 10;
+}
+class Card {
+	std::string suit;
+	std::string cardNumber;
+
+	public:
+		Card(std::string suit, std::string cardNumber) {
+			this->suit = suit;
+			this->cardNumber = cardNumber;
+		}
+
+		Card() {}
+		/// <summary>
+		/// Retrieves the suit.
+		/// </summary>
+		/// <returns>the suit</returns>
+		std::string getSuit() {
+			return this->suit;
+		}
+		/// <summary>
+		///	Retrieves the cardNumber.
+		/// </summary>
+		/// <returns>the card number</returns>
+		std::string getCardNumber() {
+			return this->cardNumber;
+		}
+
+		void readCard() {
+			std::cout << this->cardNumber << " of " << this->suit << "\n";
+		}
+};
+
+class Dealer : public Player {
+	std::queue<Card> deck;
+	std::queue<Card> hands;
+	public:
+		Card giveCardToDealer() {
+			Card c = this->removeCard();
+			this->addCard(c);
+			return c;
+		}
+
+		Card removeCard() {
+			Card c = deck.front();
+			deck.pop();
+			return c;
+		}
+
+		Dealer(int seed) {
+			std::array<Card, 312> cards;
+			std::string cardNums[] = { "Two", "Three", "Four", "Five", "Six", "Seven", "Eight",
+			"Nine", "Ten", "Jack", "Queen", "King", "Ace"};
+			std::string suits[] = { "Hearts", "Spades", "Diamonds", "Clubs" };
+			int i = 0;
+			for (int index = 0; i < 6; i++) {
+				for (std::string suit : suits) {
+					for (std::string num : cardNums) {
+						Card c(suit, num);
+						cards[i++] = c;
+					}
+				}
+			}
+			std::shuffle(cards.begin(), cards.end(), seed);
+			std::queue<Card> deck;
+			for (int j = 0; j < cards.size(); j++) {
+				Card c = cards[i];
+				deck.push(c);
+			}
+			this->deck = deck;
+		}
+};
+
+class Player {
+	int sum;
+	int aceSum;
+	float money;
+	bool isIn;
+	std::queue<Card> hand;
+
+	public:
+		Player(float money) {
+			this->sum = 0;
+			this->money = money;
+			this->isIn = true;
+			this->hand = std::queue<Card>();
+			this->aceSum = 0;
+		}
+		
+		Player() {
+			this->sum = 0;
+			this->money = 0;
+			this->hand = std::queue<Card>();
+			this->aceSum = 0;
+		}
+
+		int getSum() {
+			return this->sum;
+		}
+
+		int getAceSum() {
+			return this->aceSum;
+		}
+
+		float getMoney() {
+			return this->money;
+		}
+
+		bool getStatus() {
+			return this->isIn;
+		}
+
+		Card getCard() {
+			Card c = hand.front();
+			hand.pop();
+			return c;
+		}
+
+		void addCard(Card c) {
+			this->hand.push(c);
+			if (c.getCardNumber() != "Ace") {
+				int amount = cardToInt(c.getCardNumber());
+				this->sum += amount;
+			}
+			else {
+				this->aceSum = this->sum + 1;
+				this->sum += 10;
+			}
+			
+		}
+
+};
